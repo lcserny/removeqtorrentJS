667b6c281e1b876c282f983e12ea88e07a3a8561
Leonardo Cserny <leo.cserny@gmail.com>
2024-06-17 12:47:01 +0300

    makes json logging optional

--- src/logging.js
+++ src/logging.js
@@ -1,14 +1,18 @@
 const winston = require('winston');
-const { combine, timestamp, prettyPrint, errors } = winston.format;
+const { combine, timestamp, prettyPrint, printf, errors } = winston.format;
 
-let logger = winston.createLogger({
-    format: combine(errors({stack: true}), timestamp(), prettyPrint())
-});
+let logger = winston.createLogger();
 
 function initLogging(logConfig) {
     logger.level = logConfig?.level || "info";
+    logger.format = combine(errors({stack: true}), timestamp(), logConfig.json
+        ? prettyPrint()
+        : printf(({timestamp, level, message, stack}) => {
+            const text = `${timestamp} ${level.toUpperCase()} ${message}`;
+            return stack ? text + '\n' + stack : text;
+        }));
     logger.add(new winston.transports.Console());
-    logger.add(new winston.transports.File({filename: logConfig?.file || "removeqtorrent.log.json"}));
+    logger.add(new winston.transports.File({filename: logConfig?.file || "removeqtorrent.log"}));
 }
 
 module.exports = {
