4e268d236ccab96ef608785305f412e6bb2f1f9f
Leonardo Cserny <leo.cserny@gmail.com>
2024-07-19 07:58:03 +0300

    fixes debugging

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   testEnvironment: "node",
   preset: "ts-jest/presets/js-with-ts",
   setupFilesAfterEnv: ['./setup.js'],
+  testPathIgnorePatterns: ["/node_modules/", "/__tests__/build/", "/build/"],
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: "coverage",
